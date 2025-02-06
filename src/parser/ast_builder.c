0d1722dd717db4fc2b0127d4037fe2f2dc18b34b
Alexandre Lacroix <alacroix@paul-f5Ar4s5.clusters.42paris.fr>
2025-02-06 16:36:29 +0100

    ast wip

--- src/parser/ast_builder.c
+++ src/parser/ast_builder.c
@@ -6,7 +6,7 @@
 /*   By: alacroix <alacroix@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/06 12:33:09 by alacroix          #+#    #+#             */
-/*   Updated: 2025/02/06 15:57:12 by alacroix         ###   ########.fr       */
+/*   Updated: 2025/02/06 16:32:33 by alacroix         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,14 +38,72 @@ static char	**create_arg(t_token *token_lst, int *token_index)
 
 static t_ast	*parse_cmd(t_token *token_lst, int *token_index)
 {
+	t_ast	*current;
+	t_ast	*redir;
+
+	current = NULL;
+	redir = NULL;
+	if(token_lst[*token_index].token_type != TOKEN_TEXT)
+		return (NULL);
+	current = malloc (sizeof(t_ast));
+	if(!current)
+		return (NULL);
+	current->node_type = COMMAND;
+	current->args = create_arg(token_lst, token_index);
 }
 
 static t_ast	*parse_pipe(t_token *token_lst, int *token_index)
 {
+	t_ast	*left;
+	t_ast	*current;
+
+	left = parse_cmd(token_lst, token_index);
+	current = NULL;
+	while (token_lst[*token_index].token_type == TOKEN_PIPE)
+	{
+		current = malloc(sizeof(t_ast));
+		if (!current)
+			return (NULL);
+		current->node_type = PIPE;
+		current->args = NULL;
+		current->subshell = NULL;
+		current->redir_file = NULL;
+		current->next = NULL;
+		(*token_index)++;
+		current->left_node = left;
+		current->right_node = parse_cmd(token_lst, token_index);
+		left = current;
+	}
+	return (left);
 }
 
 static t_ast	*parse_logic_operator(t_token *token_lst, int *token_index)
 {
+	t_ast	*left;
+	t_ast	*current;
+
+	left = parse_pipe(token_lst, token_index);
+	current = NULL;
+	while (token_lst[*token_index].token_type == TOKEN_AND
+		|| token_lst[*token_index].token_type == TOKEN_OR)
+	{
+		current = malloc(sizeof(t_ast));
+		if (!current)
+			return (NULL);
+		if (token_lst[*token_index].token_type == TOKEN_AND)
+			current->node_type = AND;
+		else
+			current->node_type = OR;
+		current->args = NULL;
+		current->subshell = NULL;
+		current->redir_file = NULL;
+		current->next = NULL;
+		(*token_index)++;
+		current->left_node = left;
+		current->right_node = parse_pipe(token_lst, token_index);
+		left = current;
+	}
+	return (left);
 }
 
 t_ast	*parser(t_token *token_lst)
