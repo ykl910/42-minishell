b7e66a4585d80eb34cb03aaa88df3eab17379d08
ykl910 <64692077+ykl910@users.noreply.github.com>
2025-02-12 12:03:11 +0100

    add expansion

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: kyang <kyang@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/29 15:20:48 by kyang             #+#    #+#             */
-/*   Updated: 2025/02/11 20:07:16 by kyang            ###   ########.fr       */
+/*   Updated: 2025/02/12 10:24:50 by kyang            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -125,7 +125,7 @@ int	main(int ac, char **av, char **envp)
 		if (line)
 		{
 			add_history(line);
-			expanded_line = expand_wc(line);
+			expanded_line = expand_line(line, &shell);
 			shell.token_lst = lexer(expanded_line);
 			token = shell.token_lst;
 			// while (token)
