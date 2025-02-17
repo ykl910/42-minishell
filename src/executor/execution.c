bead04f24b236ae86132f79d380aee67f3fa6522
Kai-lin Yang <kyang@bess-f2r4s4.clusters.42paris.fr>
2025-02-17 18:57:18 +0100

    exec modif

--- src/executor/execution.c
+++ src/executor/execution.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   execution.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: alacroix <alacroix@student.42.fr>          +#+  +:+       +#+        */
+/*   By: kyang <kyang@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/11 17:03:36 by kyang             #+#    #+#             */
-/*   Updated: 2025/02/17 18:54:51 by alacroix         ###   ########.fr       */
+/*   Updated: 2025/02/17 18:59:08 by kyang            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,23 +49,21 @@ int	execute_command(t_ast_node *node, t_shell *shell)
 
 void	execute(t_ast_node *node, t_shell *shell)
 {
-	if (node->node_type == COMMAND_SIMPLE)
-		ft_printf("execute cmd: %s\n", node->value[0]); //execute_commad(node, shell);
-	else if (node->node_type == COMMAND_PIPE)
+	// if (node->node_type == COMMAND_SIMPLE)
+	// 	node->status = execute_commad(node, shell);
+	if (node->node_type == COMMAND_PIPE)
 		node->status = execute_pipe(node->left, node->right, shell);
 	else if (node->node_type == COMMAND_AND)
 	{
 		node->status = execute_command(node->left, shell);
-		//ft_printf("&&: l->cmd_type: %d\n", node->left->node_type); //execute(node->left, shell);
 		if (shell->status == 0)
 			node->status = execute_command(node->right, shell);
-			//ft_printf("&&: r->cmd_type %d\n", node->right->node_type); //executer(node->right, shell);
 	}
 	else if (node->node_type == COMMAND_OR)
 	{
-		ft_printf("||: l->cmd_type: %d\n", node->left->node_type); //execute(node->left, shell);
+		node->status = execute_command(node->left, shell);
 		if (shell->status != 0)
-			ft_printf("||: r->cmd: %d\n", node->right->node_type); //executer(node->right, shell);
+			node->status = execute_command(node->right, shell);
 	}
 	else if (node->node_type == COMMAND_SUBSHELL)
 		ft_printf("SUBSHELL\n"); //excute_subshell(node, shell);
@@ -85,7 +83,7 @@ void	inorder_traversal(t_ast_node *node, t_shell *shell)
 	//inorder_traversal(node->left, shell);
 	if (node->node_type >= COMMAND_PIPE && node->node_type <= COMMAND_OR)
 		execute(node, shell);
-//	inorder_traversal(node->right, shell);
+	//inorder_traversal(node->right, shell);
 }
 
 
