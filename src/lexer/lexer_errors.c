e71109c2812b4d50fd46cb837060ac778d3146df
ykl910 <64692077+ykl910@users.noreply.github.com>
2025-02-18 11:41:30 +0100

    modify lexer error

--- src/lexer/lexer_errors.c
+++ src/lexer/lexer_errors.c
@@ -6,13 +6,13 @@
 /*   By: kyang <kyang@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 13:56:04 by kyang             #+#    #+#             */
-/*   Updated: 2025/02/17 18:01:14 by kyang            ###   ########.fr       */
+/*   Updated: 2025/02/18 11:39:01 by kyang            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "minishell.h"
 
-void	print_syntax_error(char *token_value)
+static void	print_syntax_error(char *token_value)
 {
 	if (token_value == NULL || *token_value == '\0')
 		token_value = "newline";
@@ -22,7 +22,65 @@ void	print_syntax_error(char *token_value)
 	exit(1);
 }
 
-// pipe or and or or before a left parent
+static void	check_redirect(t_token **current)
+{
+	if ((*current)->token_type == TOKEN_REDIRECT_IN && (*current)->next && \
+	(*current)->next->token_type == TOKEN_REDIRECT_APPEND)
+		print_syntax_error(">");
+	else if (((*current)->token_type == TOKEN_REDIRECT_IN || (*current)->token_type == TOKEN_REDIRECT_OUT) && \
+	(*current)->next && (*current)->next->token_type == TOKEN_AND)
+		print_syntax_error("&");
+	else if ((*current)->token_type == TOKEN_REDIRECT_OUT && (*current)->next && \
+	(*current)->next->token_type == TOKEN_OR)
+		print_syntax_error("|");
+	else if ((*current)->token_type >= TOKEN_REDIRECT_IN && (*current)->token_type <= TOKEN_REDIRECT_APPEND)
+	{
+		if (!(*current)->next)
+			print_syntax_error(NULL);
+		else if ((*current)->next && (*current)->next->token_type != TOKEN_TEXT)
+			print_syntax_error((*current)->next->value);
+	}
+}
+
+static void	check_operator(t_token **current)
+{
+	if ((*current)->token_type == TOKEN_AND || \
+	(*current)->token_type == TOKEN_OR || (*current)->token_type == TOKEN_PIPE)
+	{
+		if ((*current)->next && ((*current)->next->token_type == TOKEN_AND || \
+		(*current)->next->token_type == TOKEN_OR || (*current)->next->token_type == TOKEN_PIPE || \
+		(*current)->next->token_type == TOKEN_RPAREN))
+			print_syntax_error((*current)->next->value);
+	}
+	else if ((*current)->token_type == TOKEN_LPAREN)
+	{
+		if ((*current)->next && ((*current)->next->token_type == TOKEN_PIPE || \
+		(*current)->next->token_type == TOKEN_AND || (*current)->next->token_type == TOKEN_OR || \
+		(*current)->next->token_type == TOKEN_RPAREN))
+			print_syntax_error((*current)->next->value);
+	}
+}
+
+static void	check_current(t_token **current, t_token **last)
+{
+	while (*current)
+	{
+		if ((*current)->next && (*current)->token_type == TOKEN_REDIRECT_IN && \
+		(*current)->next->token_type == TOKEN_REDIRECT_OUT)
+		{
+			(*current) = (*current)->next;
+			continue ;
+		}
+		check_redirect(current);
+		check_operator(current);
+		if ((*current)->next && (*current)->next->token_type == TOKEN_LPAREN && \
+		((*current)->token_type != TOKEN_PIPE && (*current)->token_type != TOKEN_AND && (*current)->token_type != TOKEN_OR))
+			print_syntax_error((*current)->next->next->value);
+		*last = *current;
+		(*current) = (*current)->next;
+	}
+}
+
 void	check_lexer(t_token **tokens)
 {
 	t_token	*current;
@@ -35,48 +93,7 @@ void	check_lexer(t_token **tokens)
 	current->token_type == TOKEN_OR || current->token_type == TOKEN_RPAREN || \
 	(current->token_type == TOKEN_LPAREN && current->next->token_type == TOKEN_RPAREN))
 		print_syntax_error(current->value);
-	while (current)
-	{
-		if (current->next && current->token_type == TOKEN_REDIRECT_IN && \
-		current->next->token_type == TOKEN_REDIRECT_OUT)
-		{
-			current = current->next;
-			continue ;
-		}
-		else if (current->token_type == TOKEN_REDIRECT_IN && current->next && \
-		current->next->token_type == TOKEN_REDIRECT_APPEND)
-			print_syntax_error(">");
-		else if ((current->token_type == TOKEN_REDIRECT_IN || current->token_type == TOKEN_REDIRECT_OUT) && \
-		current->next && current->next->token_type == TOKEN_AND)
-			print_syntax_error("&");
-		else if (current->token_type == TOKEN_REDIRECT_OUT && current->next && \
-		current->next->token_type == TOKEN_OR)
-			print_syntax_error("|");
-		else if (current->token_type >= TOKEN_REDIRECT_IN && current->token_type <= TOKEN_REDIRECT_APPEND)
-		{
-			if (!current->next)
-				print_syntax_error(NULL);
-			else if (current->next && current->next->token_type != TOKEN_TEXT)
-				print_syntax_error(current->next->value);
-		}
-		else if (current->token_type == TOKEN_AND || \
-		current->token_type == TOKEN_OR || current->token_type == TOKEN_PIPE)
-		{
-			if (current->next && (current->next->token_type == TOKEN_AND || \
-			current->next->token_type == TOKEN_OR || current->next->token_type == TOKEN_PIPE || \
-			current->next->token_type == TOKEN_RPAREN))
-				print_syntax_error(current->next->value);
-		}
-		else if (current->token_type == TOKEN_LPAREN)
-		{
-			if (current->next && (current->next->token_type == TOKEN_PIPE || \
-			current->next->token_type == TOKEN_AND || current->next->token_type == TOKEN_OR || \
-			current->next->token_type == TOKEN_RPAREN))
-				print_syntax_error(current->next->value);
-		}
-		last = current;
-		current = current->next;
-	}
+	check_current(&current, &last);
 	if (last && (last->token_type == TOKEN_PIPE || last->token_type == TOKEN_AND ||
 					last->token_type == TOKEN_OR || last->token_type == TOKEN_LPAREN))
 		print_syntax_error(last->value);
