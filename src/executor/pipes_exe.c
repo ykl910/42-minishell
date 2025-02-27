861681cee82e776a80ad6a484f7d5be57ea694ee
Alexandre Lacroix <alacroix@paul-f3Ar4s10.clusters.42paris.fr>
2025-02-27 19:02:15 +0100

    norm pipe_exec

--- src/executor/pipes_exe.c
+++ src/executor/pipes_exe.c
@@ -6,11 +6,17 @@
 /*   By: alacroix <alacroix@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/13 14:51:46 by alacroix          #+#    #+#             */
-/*   Updated: 2025/02/27 16:49:42 by alacroix         ###   ########.fr       */
+/*   Updated: 2025/02/27 18:49:33 by alacroix         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+static void	handle_error(t_ast_node *node, t_shell *shell)
+{
+	node->status = 1;
+	free_exit(&node->status, shell);
+}
 
 void	new_process(t_ast_node *node, t_shell *shell)
 {
@@ -18,15 +24,9 @@ void	new_process(t_ast_node *node, t_shell *shell)
 
 	temp_env_array = NULL;
 	if (node->redir_in && node->infile_fd == -1)
-	{
-		node->status = 1;
-		free_exit(&node->status, shell);
-	}
+		handle_error(node, shell);
 	if (node->redir_out && node->outfile_fd == -1)
-	{
-		node->status = 1;
-		free_exit(&node->status, shell);
-	}
+		handle_error(node, shell);
 	if (node->status == 0)
 	{
 		if (built_in_exec(shell, node) == -1)
@@ -43,30 +43,21 @@ void	new_process(t_ast_node *node, t_shell *shell)
 	free_exit(&node->status, shell);
 }
 
-void	pipe_forker(int *pipe_fd, t_ast_node *node, t_shell *shell,
+static void	pipe_forker(int *pipe_fd, t_ast_node *node, t_shell *shell,
 		int input_fd)
 {
 	pid_t	pid;
 
 	if (pipe(pipe_fd) == -1)
-	{
-		node->status = 1;
-		free_exit(&node->status, shell);
-	}
+		handle_error(node, shell);
 	pid = fork();
 	if (pid == -1)
-	{
-		node->status = 1;
-		free_exit(&node->status, shell);
-	}
+		handle_error(node, shell);
 	if (pid == 0)
 	{
 		close(pipe_fd[0]);
 		if (dup2(pipe_fd[1], STDOUT_FILENO) == -1)
-		{
-			node->status = 1;
-			free_exit(&node->status, shell);
-		}
+			handle_error(node, shell);
 		close(pipe_fd[1]);
 		execute_pipeline(node->left, shell, input_fd);
 		free_exit(&node->left->status, shell);
@@ -76,7 +67,7 @@ void	pipe_forker(int *pipe_fd, t_ast_node *node, t_shell *shell,
 		close(input_fd);
 }
 
-int	cmd_forker(t_ast_node *node, t_shell *shell, int input_fd)
+static int	cmd_forker(t_ast_node *node, t_shell *shell, int input_fd)
 {
 	pid_t	pid;
 	int		status;
@@ -85,22 +76,16 @@ int	cmd_forker(t_ast_node *node, t_shell *shell, int input_fd)
 	redir_std(&node);
 	pid = fork();
 	if (pid == -1)
-		exit(EXIT_FAILURE);
+		handle_error(node, shell);
 	if (pid == 0)
 	{
 		if (input_fd != STDIN_FILENO && !node->redir_in)
 		{
 			if (dup2(input_fd, STDIN_FILENO) == -1)
-			{
-				node->status = 1;
-				free_exit(&node->status, shell);
-			}
+				handle_error(node, shell);
 		}
 		new_process(node, shell);
-		{
-			node->status = 1;
-			free_exit(&node->status, shell);
-		}
+		handle_error(node, shell);
 	}
 	if (input_fd != STDIN_FILENO)
 		close(input_fd);
@@ -110,7 +95,7 @@ int	cmd_forker(t_ast_node *node, t_shell *shell, int input_fd)
 
 int	execute_pipeline(t_ast_node *node, t_shell *shell, int input_fd)
 {
-	int	pipe_fd[2];
+	int pipe_fd[2];
 
 	if (!node)
 		return (node->status);
@@ -122,57 +107,3 @@ int	execute_pipeline(t_ast_node *node, t_shell *shell, int input_fd)
 	else
 		return (cmd_forker(node, shell, input_fd));
 }
-
-// int	execute_pipeline(t_ast_node *node, t_shell *shell, int input_fd)
-// {
-// 	int		pipe_fd[2];
-// 	pid_t	pid;
-// 	int		status;
-
-// 	if (!node)
-// 		return (node->status);
-// 	if (node->node_type == COMMAND_PIPE)
-// 	{
-// 		if (pipe(pipe_fd) == -1)
-// 			exit(EXIT_FAILURE);
-// 		pid = fork();
-// 		if (pid == -1)
-// 			exit(EXIT_FAILURE);
-// 		if (pid == 0)
-// 		{
-// 			close(pipe_fd[0]);
-// 			if (dup2(pipe_fd[1], STDOUT_FILENO) == -1)
-// 				exit(EXIT_FAILURE);
-// 			close(pipe_fd[1]);
-// 			execute_pipeline(node->left, shell, input_fd);
-// 			exit(EXIT_FAILURE);
-// 		}
-// 		close(pipe_fd[1]);
-// 		if (input_fd != STDIN_FILENO)
-// 			close(input_fd);
-// 		return (execute_pipeline(node->right, shell, pipe_fd[0]));
-// 	}
-// 	else
-// 	{
-// 		cmd_builder(node);
-// 		redir_std(&node);
-// 		pid = fork();
-// 		if (pid == -1)
-// 			exit(EXIT_FAILURE);
-// 		if (pid == 0)
-// 		{
-// 			if (input_fd != STDIN_FILENO)
-// 			{
-// 				if (dup2(input_fd, STDIN_FILENO) == -1)
-// 					exit(EXIT_FAILURE);
-// 				close(input_fd);
-// 			}
-// 			new_process(node, shell);
-// 			exit(EXIT_FAILURE);
-// 		}
-// 		if (input_fd != STDIN_FILENO)
-// 			close(input_fd);
-// 		waitpid(pid, &status, 0);
-// 		return (get_return_value(&status));
-// 	}
-// }
